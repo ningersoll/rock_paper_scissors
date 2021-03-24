eb83aae4f11e92e93dcf66215ddba54088bad38b
Nathan Ingersoll <ingersoll.nathanpaul@gmail.com>
2021-03-24 15:19:54 -0500

    update

---
+++ myScript.js
@@ -0,0 +1,30 @@
+function computerPlay() {
+    let choice = ["rock", "paper", "scissors"];
+   let randomPick = choice[Math.floor(Math.random() * choice.length)];
+   return(randomPick);
+}
+
+function playRound() {
+    let playerSelection = prompt("Pick rock, paper, or scissors").toLowerCase();
+    let computerSelection = computerPlay();
+    
+    console.log (`You: ${playerSelection} computer: ${computerSelection}`)
+ 
+   if (playerSelection === "rock" && computerSelection === "scissors") { 
+       console.log(`You win, ${playerSelection} beats ${computerSelection}`);
+   } else if (playerSelection === computerSelection) {
+       console.log("TIE")
+   } else {
+    console.log("You lose");
+}
+}
+
+function game() {
+    playRound()
+    playRound()
+    playRound()
+    playRound()
+    playRound()
+}
+
+game();
